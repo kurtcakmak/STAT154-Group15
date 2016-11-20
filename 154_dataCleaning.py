9625263a2e2349c4ec20f2cff40485f8568ae8f9
kurtcakmak <kcakmak@berkeley.edu>
2016-11-20 03:30:02 -0800

    Add files via upload

--- 154_dataCleaning.py
+++ 154_dataCleaning.py
@@ -15,6 +15,8 @@ import string
 from string import digits
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+import numpy as np
+import csv
 
 # read data in as data frame
 st = pd.read_csv('/Users/kurtcakmak1/Desktop/Stat154/FinalProject/HRC_train.tsv', sep = 'str("") | str("\t")',  names=['content'], engine='python')
@@ -48,21 +50,104 @@ df['token'] = df.content_non_punc.apply(nltk.word_tokenize)
 #remove stopwords + stemming
 df['new_token'] = df.token
 stop_words = set(stopwords.words('english'))
+stop_words.update(['fw', 'subject', 'case', 'doc', 'unclassified', 'a','b','c','d','e','f','g','h''i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
 porter = PorterStemmer()
 for i in df.index:
 	email = df.token[i] 
 	list_of_words = [porter.stem(j.lower()) for j in email if j.lower() not in stop_words]
 	df.new_token[i] = list_of_words
-print(df.new_token)
+#print(df.new_token[0])
+#print(len(df.new_token[0]))
+
+#Manually remove single letters, insignificant words
+#df['test'] = df.new_token
+#stop_words.update(['fw', 'unclassifi', 'a','b','c','d','e','f','g','h''i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
+#for i in df.index:
+#	email = df.new_token[i] #
+#	list_of_words = [j for j in email if j not in stop_words]
+#	df.test[i] = list_of_words	
+#print(df.new_token[0])
+#print(df.test[0])
+#print(len(df.new_token[0]))
+#print(len(df.test[0]))
 
 # create a dictionary in the form {word1:count1,...}
 dictionary = df.new_token.apply(Counter)
-print(dictionary[0])
+
+#keys = list(dictionary[0].keys())
+#print(len(keys))
+#dic = dictionary[0]
+#keys1 = keys[0]
+#print(dic[keys1])
+
+#print(dictionary[0])
 #df.content[0] = df.new_token[0]
 #print(df)
 #print(dictionary)
 
-# convert the dictionary into a data frame
-#dictionary.df = pd.DataFrame(dictionary, columns = ['label',)
+# sum up all counters
+sum_dictionary = dictionary[0]
+for index in range(1, len(df.index)):
+    sum_dictionary = sum_dictionary + dictionary[index]
+unique_words = sum_dictionary.keys()
+#print(sum_dictionary)
+#print(len(df.index))
+#print(len(sum_dictionary))
+#print(len(unique_words))
+#print(unique_words)
 
-# analyze data frame to find power feature
+# convert the dictionary into a data frame
+#w = len(df.index) + 1
+#h = len(unique_words)
+#w=3
+#h=5
+#Matrix = [[0 for x in range(w)] for y in range(h)]
+unique_words = list(unique_words)
+Matrix = np.zeros((len(df.index),len(unique_words)))
+#print(Matrix)
+#unique_words = unique_words[0:5]
+#print(Matrix[0])
+#print(len(Matrix[0]))
+#print(len(Matrix[1:w]))
+#np.set_printoptions(threshold=np.nan)
+def makeFrame(Matrix, dictionary, unique_words):
+	for j in range(len(df.index)):
+		for key in list(dictionary[j].keys()):
+			for i in range(len(unique_words)):
+				if str(unique_words[i]) == str(key):
+					count = dictionary[j][key]
+					Matrix[j,i] = int(count)					
+	return(Matrix)
+
+def makeFrame2(Matrix, dictionary, unique_words):
+	for key in list(dictionary[0].keys()):
+		for i in range(len(unique_words)):
+			if str(unique_words[i]) == str(key):
+					#print(dictionary[0][key])
+				count = dictionary[0][key]
+				Matrix[0,i] = int(count)
+					#print(Matrix[0,i])
+	return(Matrix)
+
+#a=makeFrame(Matrix, dictionary, unique_words)
+a=makeFrame2(Matrix, dictionary, unique_words)
+with open("output.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerows(a)
+#data_frame = pd.DataFrame(a)
+#data_frame.to_csv("154data.csv", sep='\t')
+
+print(a)
+#print(len(a[0]))
+#print(a[a>0])
+#print(len(a))
+
+count = 0
+for i in range(len(a[0])):
+	if a[0,i] > 0:
+		count += 1
+print(count)
+print(len(dictionary[0].keys()))
+
+
+# analyze data frame to find 1power feature
